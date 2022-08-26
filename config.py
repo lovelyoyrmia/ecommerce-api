11537f4fa4720e7dae673c01129cbf7b7c771f4d
lovelyoyrmia <mokalulovelyo@gmail.com>
2022-08-26 16:20:15 +0700

    changed environment

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 def create_app():
     load_dotenv()
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.secret_key = "secret string"
 
